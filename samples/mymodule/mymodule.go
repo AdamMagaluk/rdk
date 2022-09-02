6c99cdb8c3b1f4dc2807b23186df15c37b5dc464
James Otting <james@viam.com>
2022-09-01 17:04:28 -0700

    Modules working, need reverse connection

--- samples/mymodule/mymodule.go
+++ samples/mymodule/mymodule.go
@@ -8,15 +8,19 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/edaniels/golog"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/structpb"
 
 	"go.viam.com/utils"
 
+	pb "go.viam.com/api/proto/viam/module/v1"
+	"go.viam.com/rdk/component/motor"
+	"go.viam.com/rdk/config"
+	rdkclient "go.viam.com/rdk/grpc/client"
 	pbgeneric "go.viam.com/rdk/proto/api/component/generic/v1"
-	pb "go.viam.com/rdk/proto/api/module/v1"
+	"go.viam.com/rdk/resource"
 	//"go.viam.com/rdk/config"
-	"go.viam.com/rdk/protoutils"
 )
 
 type myComponent struct {
@@ -40,14 +44,37 @@ type server struct {
 }
 
 
-func (s *server) AddResource(ctx context.Context, req *pb.AddResourceRequest) (*pb.AddResourceResponse, error) {
-	log.Printf("SMURF100: %+v", req)
+func (s *server) AddComponent(ctx context.Context, req *pb.AddComponentRequest) (*pb.AddComponentResponse, error) {
+	cfg, err := config.ComponentConfigFromProto(req.Config)
+	if err != nil {
+		return &pb.AddComponentResponse{}, err
+	}
+	log.Printf("Config: %+v", cfg)
+	log.Printf("Deps: %+v", req.Dependencies)
 
-	name := protoutils.ResourceNameFromProto(req.Name)
-	conf := req.Config.AsMap()
-	log.Printf("SMURF101: %+v, %+v", name, conf)
+	if len(req.Dependencies) > 0 {
+		rc, err := rdkclient.New(context.Background(), "localhost:8080", logger)
+		if err != nil {
+			logger.Error(err)
+		}
+		rName, _ := resource.NewFromString("rdk:component:motor/beepbeep")
+		if err != nil {
+			logger.Error(err)
+		}
+		m, err := rc.ResourceByName(rName)
+		if err != nil {
+			logger.Error(err)
+		}
+		logger.Debugf("Component1: %+v", m)
+		mreal, ok := m.(motor.Motor)
+		logger.Debugf("Component2: %+v, %+v", mreal, ok)
+	}
 
-	return &pb.AddResourceResponse{}, nil
+	return &pb.AddComponentResponse{}, nil
+}
+
+func (s *server) Ready(ctx context.Context, req *pb.ReadyRequest) (*pb.ReadyResponse, error) {
+	return &pb.ReadyResponse{Ready: true}, nil
 }
 
 // Arguments for the command.
@@ -55,16 +82,18 @@ type Arguments struct {
 	Socket         string `flag:"0,required,usage=socket path"`
 }
 
+var logger = golog.NewDevelopmentLogger("MyModule")
+
 func main() {
+	// f, err := os.OpenFile("/tmp/mod.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	// if err != nil {
+	// 	log.Fatalf("error opening file: %v", err)
+	// }
+	// defer f.Close()
+	//log.SetOutput(f)
 
-	f, err := os.OpenFile("/tmp/mod.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
 
-	log.SetOutput(f)
-
+	var err error
 
 	var argsParsed Arguments
 	if err := utils.ParseFlags(os.Args, &argsParsed); err != nil {
